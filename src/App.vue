c8fcbf1083f115ec691669107c4ae87508cd2a82
anishghimire <anishghimire862@gmail.com>
2020-04-13 20:28:04 +0545

    Added vue-router and home page.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
   <div id="app">
-    aa
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      navItems: [
+        { title: 'Home', link: '/', isActive: true },
+        { title: 'Blog', link: '/blog', isActive: false },
+        { title: 'Education', link: '/education', isActive: false },
+        { title: 'Projects', link: '/projects', isActive: false },
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+
+import HomePage from './pages/Home.vue'
+
+const routes = [
+  { path: '/', component: HomePage }
+];
+
+export default routes;
