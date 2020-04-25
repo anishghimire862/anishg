496d86e1eadfba64ced07fb683004aacce380e1a
anishghimire <anishghimire862@gmail.com>
2020-04-25 23:54:02 +0545

    Added blog route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 
 import HomePage from './pages/Home.vue'
 import BlogPage from './pages/Blog.vue'
+import BlogPost from './pages/blog/blog_post.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/blog', component: BlogPage }
+  { path: '/blog', component: BlogPage },
+  { path: '/blog/:title', component: BlogPost }
 ];
 
 export default routes;
