a7609643400a885f06aa0574fcdaf2432ee5c95d
anishghimire <anishghimire862@gmail.com>
2020-04-26 00:38:46 +0545

    Added education section

---
+++ src/pages/Education.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="row pl-4 pr-4"
+  >  
+    <div
+      v-for="education in institutions"
+      :key="education.institute"
+      class="col-sm-12 col-md-4 d-flex justify-content-center"
+    >
+      <div class="card border-light mt-3" style="width: 100%;">
+        <div class="card-header">
+          {{ education.period }}
+        </div>
+        <div class="card-body">
+          <h6 class="card-title font-weight-bold">
+            {{ education.institute }}
+          </h6>
+          <p class="card-text" style="color: grey">
+            {{ education.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import educationData from '../json/education.json'
+export default {
+  data () {
+    return {
+      institutions: educationData
+    }
+  }
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@
 import HomePage from './pages/Home.vue'
 import BlogPage from './pages/Blog.vue'
 import BlogPost from './pages/blog/blog_post.vue'
+import EducationPage from './pages/Education.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/blog', component: BlogPage },
-  { path: '/blog/:title', component: BlogPost }
+  { path: '/blog/:title', component: BlogPost },
+  { path: '/education', component: EducationPage },
 ];
 
 export default routes;
