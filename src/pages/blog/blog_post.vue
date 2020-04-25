1a77e24b18f3de910f218677c75dda32ba05d38e
anishghimire <anishghimire862@gmail.com>
2020-04-25 23:53:20 +0545

    Added blog view page

--- src/pages/blog/blog_post.vue
+++ src/pages/blog/blog_post.vue
@@ -6,8 +6,6 @@
       </div>
       <div class="col-sm-12 col-md-6 p-0" style="background-color: #fff;">
         <div
-          v-for="blog in blogs"
-          :key="blog.title" 
           class="card border border-white pl-5 pr-5 pt-3 pb-0"
           style="max-width: 100%"
         >
@@ -15,32 +13,22 @@
             <small
               class="font-weight-bold"
             > 
-              {{ blog.createdAt }} 
+              {{ blog[0].createdAt }}
               <sup>
                 <span class="font-weight-bold"> . </span>
               </sup> 
               <small
                 class="font-weight-bold blog-category"
               > 
-                {{ blog.category }}
+                {{ blog[0].category }}
               </small>
             </small>
             <h5 class="card-title font-weight-bold mb-1">
-              {{ blog.title }}
+              {{ blog[0].title }}
             </h5>
             <p class="card-text blog-content mb-1">
-              {{ blog.content }}
+              {{ blog[0].content }}
             </p>
-            <router-link
-              :to="`/blog/${blog.title}`"
-            >
-              <span
-                class="read-more"
-
-              > 
-                READ
-              </span>
-            </router-link>
           </div>
         </div>
       </div>
@@ -48,13 +36,24 @@
   </div>
 </template>
 <script>
-import blogData from '../json/blog.json'
+import blogData from '../../json/blog.json'
 export default {
   data () {
     return {
-      blogs: blogData
+      blogs: blogData,
+      blog: null,
+      title: this.$route.params.title
     }
   },
+  mounted () {
+    this.getBlog();
+  },
+  methods: {
+    getBlog () {
+      this.blog =  this.blogs.filter(blog => blog.title === this.title)
+      return this.blog
+    }
+  }
 }
 </script>
 <style scoped>
@@ -63,8 +62,5 @@ export default {
 }
 .blog-content {
   color: grey;
-}
-.read-more {
-  color: blue;
 }
 </style>
