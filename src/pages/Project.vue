b1478b3fef41680db07c7a936e28ce4d5d0cad3d
anishghimire <anishghimire862@gmail.com>
2020-04-26 00:43:15 +0545

    Added project page

---
+++ src/pages/Project.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 
+    <div class="alert alert-primary m-3" role="alert">
+      Coming soon...
+    </div>
+  </div>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import HomePage from './pages/Home.vue'
 import BlogPage from './pages/Blog.vue'
 import BlogPost from './pages/blog/blog_post.vue'
 import EducationPage from './pages/Education.vue'
+import ProjectPage from './pages/Project.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/blog', component: BlogPage },
   { path: '/blog/:title', component: BlogPost },
   { path: '/education', component: EducationPage },
+  { path: '/projects', component: ProjectPage }
 ];
 
 export default routes;
