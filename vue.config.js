f109d7de315105bf2122deedada2d52f910ec99d
Anish Ghimire <anishghimire862@gmail.com>
2020-04-29 15:43:00 +0545

    Added prerender spa config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+var path = require('path')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/anishg/'
     : '/'
+  ,
+  plugins: [
+    new PrerenderSpaPlugin(
+      // Absolute path to compiled SPA
+      path.join(__dirname, '.'),
+      // List of routes to prerender
+      ['/', '/blog', '/education', '/projects']
+    )
+  ]
 }
 
---
+++ webpack.conf.js
@@ -0,0 +1,13 @@
+var path = require('path')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+module.exports = {
+  // ...
+  plugins: [
+    new PrerenderSpaPlugin(
+      // Absolute path to compiled SPA
+      path.join(__dirname, '.'),
+      // List of routes to prerender
+      ['/', '/blog', '/education', '/projects']
+    )
+  ]
+}
