aa05dd1555d72690674e5dcb7d2ffc622f0855c8
Anish Ghimire <anishghimire862@gmail.com>
2020-04-29 19:11:41 +0545

    Updated prerender config

--- vue.config.js
+++ vue.config.js
@@ -7,30 +7,14 @@ module.exports = {
     : '/'
   ,
   configureWebpack: {
-    headless: false,
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        inject: false
-      }),
-      new PrerenderSpaPlugin({
-        staticDir: path.resolve(__dirname, './dist'),
-        routes: [ '/', '/blog', '/education', '/projects'],
-        postProcess(context) {
-          let titles = {
-            '/': 'My home page',
-            '/blog': 'My awesome about page',
-            '/education': 'Contact me',
-            '/projects': 'Contact me'
-          };
-          context.html = context.html.replace(
-            /<title>[^<]*<\/title>/i,
-            `<title>${titles[context.route]}</title>`
-          )
-          return context
-        }
+    plugins: process.env.NODE_ENV === 'production' ? [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/', '/education', '/projects', '/blog'],
       })
-    ]
+    ] : []
   }
 }
 
