8f63ade9de2d9d9c5816d5013155814e5810ae9a
Anish Ghimire <anishghimire862@gmail.com>
2020-04-29 19:34:45 +0545

    Updated prerender config

--- vue.config.js
+++ vue.config.js
@@ -6,19 +6,22 @@ module.exports = {
     ? '/anishg/'
     : '/'
   ,
-  configureWebpack: {
-    plugins: process.env.NODE_ENV === 'production' ? [
-      new PrerenderSpaPlugin({
-        // Required - The path to the webpack-outputted app to prerender.
-        staticDir: path.join(__dirname, 'dist'),
-        // Required - Routes to render.
-        routes: [ '/', '/education', '/projects', '/blog'],
-        renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
-          renderAfterDocumentEvent: 'render-event',
-          headless: false,
-        }),
-      })
-    ] : []
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/about'
+      ],
+      useRenderEvent: true,
+      onlyProduction: true,
+ 
+      headless: false, // <- this could also be inside the customRendererConfig
+      customRendererConfig:
+      {
+        args: ["--auto-open-devtools-for-tabs"]
+      }
+    }
   }
 }
 
