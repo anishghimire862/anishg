834dfd74ac3ecddd8aa224275309749c075f9317
Anish Ghimire <anishghimire862@gmail.com>
2020-04-29 20:11:12 +0545

    Updated prerender config

--- vue.config.js
+++ vue.config.js
@@ -5,24 +5,26 @@ module.exports = {
     ? '/anishg/'
     : '/'
   ,
-  plugins: [
-    new PrerenderSPAPlugin(
-      path.join(__dirname, 'relative/path/to/static/root'),
-      [ '/', '/about', '/education' ],
-      {
-        postProcessHtml: function (context) {
-          var titles = {
-            '/': 'Home',
-            '/about': 'Our Story',
-            '/education': 'Contact Us'
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin(
+        path.join(__dirname, 'relative/path/to/static/root'),
+        [ '/', '/about', '/education' ],
+        {
+          postProcessHtml: function (context) {
+            var titles = {
+              '/': 'Home',
+              '/about': 'Our Story',
+              '/education': 'Contact Us'
+            }
+            return context.html.replace(
+              /<title>[^<]*<\/title>/i,
+              '<title>' + titles[context.route] + '</title>'
+            )
           }
-          return context.html.replace(
-            /<title>[^<]*<\/title>/i,
-            '<title>' + titles[context.route] + '</title>'
-          )
         }
-      }
-    )
-  ]
+      )
+    ]
+  }
 }
 
